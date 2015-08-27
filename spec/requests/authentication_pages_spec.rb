74118fadc2b39936e3a3e2af31a3e99d768350f5
shun91 <shun9167@gmail.com>
2015-08-27 19:41:53 +0900

    finish sec 9 exercises

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -62,6 +62,20 @@ describe "Authentication" do
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
           end
+
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
+            end
+          end
         end
       end
 
@@ -80,6 +94,14 @@ describe "Authentication" do
         describe "visiting the user index" do
           before { visit users_path }
           it { should have_title('Sign in') }
+        end
+
+        describe "not exists link" do
+          before { visit root_path }
+          it { should_not have_link('Profile',  href: user_path(user)) }
+          it { should_not have_link('Settings', href: edit_user_path(user)) }
+          it { should_not have_link('Sign out', href: signout_path) }
+          it { should have_link('Sign in',      href: signin_path) }
         end
       end
     end
@@ -112,5 +134,16 @@ describe "Authentication" do
         specify { expect(response).to redirect_to(root_path) }
       end
     end
+
+    describe "as admin user" do
+      let(:admin) { FactoryGirl.create(:admin) }
+      before { sign_in admin, no_capybara: true }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(admin) }
+        specify { expect(response).to redirect_to(root_path) }
+      end
+   end
+
   end
 end
